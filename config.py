1e2b0c004d4c471286d6bb4c7c0a65ec52685024
cryptalmist <cryptalmist@gmail.com>
2024-09-14 11:58:05 +0700

    ABC

---
+++ config.py
@@ -0,0 +1,7 @@
+bot_link = 'https://top.gg/bot/664508672713424926/vote' #Enter bot link you wish to vote
+# use_cookies = True
+use_token = False #wip
+accounts = [
+    {"email": "w", "password": "-!A3.q"},
+    # Add more accounts as needed
+]
